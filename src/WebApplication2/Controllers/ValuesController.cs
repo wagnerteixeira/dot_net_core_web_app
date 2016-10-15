ff9ca56750e324e94118829db7fa4ad44a3d6987
DCC Hackthon - Time D - Usuario 5 <hackthon20@dcc.ufmg.br>
2016-10-15 16:54:58 -0300

    Inicial

---
+++ src/WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private static SampleContext _dbcontext = EmployeesContextFactory.Create();
+
+        private DbSet<SampleTable> DbSet
+        {
+            get { return _dbcontext.Set<SampleTable>(); }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<SampleTable> Get()
+        {
+            return DbSet;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public SampleTable Get(int id)
+        {
+            return DbSet.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody]SampleTable value)
+        {
+            try
+            {
+                DbSet.Add(value);
+                _dbcontext.SaveChanges();
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                return "Erro: " + e.Message;
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody]SampleTable value)
+        {
+            try
+            {
+                SampleTable t = DbSet.Where(c => c.Id == id).FirstOrDefault();
+                t.Name = value.Name;
+                _dbcontext.Entry<SampleTable>(t).State = EntityState.Modified;
+                _dbcontext.SaveChanges();
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                return "Erro: " + e.Message;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            SampleTable t = DbSet.Where(c => c.Id == id).FirstOrDefault();
+            if (t != null)
+            {
+                DbSet.Remove(t);
+                _dbcontext.SaveChanges();
+                return "OK";
+            }
+            else
+            {
+                return "Não encontrado";
+            }
+        }
+    }
+}
---
+++ src/WebApplication2/SampleContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2
+{
+    public class SampleContext : DbContext
+    {
+        public static string ConnectionString { get; set; }
+
+        public SampleContext(DbContextOptions<SampleContext> options)
+        : base(options)
+        { }
+
+        public DbSet<SampleTable> SampleTables { get; set; }
+    }
+
+    /// <summary>
+    /// Factory class for EmployeesContext
+    /// </summary>
+    public static class EmployeesContextFactory
+    {
+        public static SampleContext Create()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SampleContext>();
+            optionsBuilder.UseMySQL(SampleContext.ConnectionString);
+
+            //Ensure database creation
+            var context = new SampleContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+
+    /// <summary>
+    /// A basic class for an Employee
+    /// </summary>
+    public class SampleTable
+    {
+        public SampleTable()
+        {
+        }
+
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
